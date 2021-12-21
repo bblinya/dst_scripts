8144f35cc0ca4b9f1ffeb52f64f40a3738bd8417
Beta Beauty <cunxinshuihua@gmail.com>
2021-12-21 10:55:16 +0800

    [stash]:

--- remove_archives.sh
+++ remove_archives.sh
@@ -4,13 +4,13 @@ source common.sh
 
 
 if [ -d "$CLUSTER_PATH/Master" ]; then
-	echo "Remove DST forest archieve"
+	echo "Remove DST forest archieve."
 	rm -rf "$CLUSTER_PATH/Master/save/session"
 	rm -rf "$CLUSTER_PATH/Master/save/server_temp"
 fi
 
 if [ -d "$CLUSTER_PATH/Caves" ]; then
-	echo "Remove DST cave archieve"
+	echo "Remove DST cave archieve."
 	rm -rf "$CLUSTER_PATH/Caves/save/session"
 	rm -rf "$CLUSTER_PATH/Caves/save/server_temp"
 fi
